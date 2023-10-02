ff5538d66687597a110ab87fe0e5c890c7e2fa5f
ornob90 <oornob49@gmail.com>
2023-10-02 10:12:47 +0600

    responsive newsfeed complete

--- src/pages/RecipeFeed/RecipeFeed.jsx
+++ src/pages/RecipeFeed/RecipeFeed.jsx
@@ -7,10 +7,10 @@ import RecipePost from "../../components/Shared/RecipePost";
 const RecipeFeed = () => {
   return (
     <Container>
-      <div className=" bg-white sticky top-[8%] sm:top-[15%] md:top-[11%] grid grid-cols-1 md:grid-cols-2 w-full place-content-center">
-        <div className="relative pt-4 flex ">
+      <div className=" bg-white sticky top-[8%] sm:top-[15%] md:top-[11%] grid grid-cols-1 md:grid-cols-2 w-full place-content-center pb-2 sm:pb-4 pt-9 md:pt-6 lg:py-4">
+        <div className="relative pt-4 flex z-[1]">
           <input
-            className=" border focus:outline-none w-full py-3 pl-5 rounded-l-lg text-md "
+            className=" border focus:outline-none w-full py-2 sm:py-3 pl-4 sm:pl-5 rounded-l-lg text-sm sm:text-md "
             type="text"
             placeholder="Search Your Food"
           />

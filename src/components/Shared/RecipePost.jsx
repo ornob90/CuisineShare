37fc4c2e2412d5c9c38e2e388154588e3ebfab7b
ornob90 <oornob49@gmail.com>
2023-10-02 14:18:21 +0600

    adding like and bookmark button in posts

--- src/components/Shared/RecipePost.jsx
+++ src/components/Shared/RecipePost.jsx
@@ -1,20 +1,28 @@
 import { Rating } from "@mui/material";
 import React from "react";
 import Button from "./Button";
+import { FaRegBookmark, FaBookmark } from "react-icons/fa";
+import { AiOutlineHeart, AiTwotoneHeart } from "react-icons/ai";
 
 const RecipePost = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
       <div className="w-full md:w-auto">
         <img
           src="https://www.teenaagnel.com/wp-content/uploads/2019/12/food-photography-in-dubai.jpg"
           alt=""
-          className="rounded-lg w-full  object-cover"
+          className="object-cover w-full h-full rounded-lg"
         />
       </div>
 
-      <div className="w-full md:w-auto flex flex-col gap-2 justify-between">
-        <h1 className="text-3xl font-bold mb-2">Burger</h1>
+      <div className="flex flex-col justify-between w-full gap-2 md:w-auto">
+        <div className="flex justify-between">
+          <h1 className="mb-2 text-3xl font-bold">Burger</h1>
+          <div className="flex gap-4 text-2xl">
+            <AiOutlineHeart className="text-3xl" />
+            <FaRegBookmark />
+          </div>
+        </div>
         <Rating className="pt-4 -z-1" name="read-only" value={3.5} readOnly />
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio,
@@ -22,7 +30,7 @@ const RecipePost = () => {
           cumque explicabo.
         </p>
         <p>
-          <span>Cooking Time: </span> 1:20 Hour
+          <span className="font-bold">Cooking Time: </span> 1:20 Hour
         </p>
         <div className="flex items-center gap-4">
           <div className="badge badge-outline">Italian</div>
--- src/components/Shared/Select.jsx
+++ src/components/Shared/Select.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Select = ({ name, options }) => {
   return (
     <div>
-      <select className="select select-primary w-full max-w-xs border-none focus:outline-none">
+      <select className="w-full max-w-xs border-none select select-primary focus:outline-none">
         <option defaultValue>{name}</option>
         {options.map((option) => (
           <option key={option}>{option}</option>

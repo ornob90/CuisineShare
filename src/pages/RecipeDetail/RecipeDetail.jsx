b2888673f02269439b4b26ec442a6322b8476bc9
ornob90 <oornob49@gmail.com>
2023-10-02 15:06:25 +0600

    adding review design in the detail page

--- src/pages/RecipeDetail/RecipeDetail.jsx
+++ src/pages/RecipeDetail/RecipeDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../../components/Shared/Container";
 import { Rating } from "@mui/material";
+import Button from "../../components/Shared/Button";
 
 const RecipeDetail = () => {
   return (
@@ -36,6 +37,31 @@ const RecipeDetail = () => {
             <span className="font-bold">Ingredients </span> soy sauce, water,
             brown sugar, ground ginger, minced garlic, cornsarch, chicken garlic
           </p>
+        </div>
+        <div className="z-[1] relative w-[90%] sm:w-[55%] pt-4 flex">
+          <input
+            className="w-full py-3 pl-5 border rounded-l-lg focus:outline-none text-md"
+            type="text"
+            placeholder="Comment"
+          />
+          <Button classes="bg-black py-3 px-8 text-sm rounded-r-lg text-white ">
+            Comment
+          </Button>
+        </div>
+        <div className="mt-20 space-y-10">
+          {[1, 2, 3, 4, 5].map((e) => (
+            <div key={e}>
+              <div className="flex items-center gap-4">
+                <div className="h-[40px] w-[40px] rounded-full border-black border-2"></div>
+                <p>Ornob</p>
+              </div>
+              <p className="w-[60%] mt-5">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+                mollitia, amet rerum natus reiciendis suscipit officiis ea unde
+                neque ipsum!
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </Container>

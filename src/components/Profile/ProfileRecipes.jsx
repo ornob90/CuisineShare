6af7f9763379c587e3cd497d49a68751f0892e5d
ornob90 <oornob49@gmail.com>
2023-10-05 11:00:21 +0600

    dynamic profile recipes complete

--- src/components/Profile/ProfileRecipes.jsx
+++ src/components/Profile/ProfileRecipes.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipePost from "../Shared/RecipePost";
+import useDb from "../../hooks/useDb";
+import useAuth from "../../hooks/useAuth";
 
 const ProfileRecipes = () => {
+  const [profileRecipes, setProfileRecipes] = useState([]);
+
+  const { posts } = useDb();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    setProfileRecipes(
+      Object.keys(posts).filter((post) => posts[post].userEmail === user.email)
+    );
+
+    // // console.log(posts);
+  }, [posts]);
+
   return (
     <div className="mt-20 grid grid-cols-1 gap-10">
-      <RecipePost />
-      <RecipePost />
-      <RecipePost />
-      <RecipePost />
-      <RecipePost />
-      <RecipePost />
-      <RecipePost />
+      {profileRecipes.map((post) => (
+        <RecipePost key={posts[post].id} post={posts[post]} />
+      ))}
     </div>
   );
 };
